898d70706e412662507a5debcee6ddae79bf4950
ViniSilv27 <vinicius.s.silv18@gmail.com>
2025-11-15 15:34:53 -0300

    encorporando o styled-components

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,7 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
-
+import { BotaoPesquisa, Campo, FormularioVagas } from './FormVagas'
 type Props = {
   aoPesquisar: (termo: string) => void
 }
@@ -15,17 +13,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormularioVagas onSubmit={aoEnviarForm}>
+      <Campo
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisa type="submit">Pesquisar</BotaoPesquisa>
+    </FormularioVagas>
   )
 }
 export default FormVagas
--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Vaga.module.css'
+import { VagaShow, VagasLink, VagasTitulo } from './Vaga'
 
 type Props = {
   titulo: string
@@ -11,8 +11,8 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <VagaShow>
+    <VagasTitulo>{props.titulo}</VagasTitulo>
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +22,8 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+    <VagasLink href="#">Ver detalhes e candidatar-se</VagasLink>
+  </VagaShow>
 )
 
 export default Vaga
